b9979b65fc93dc4030b778d69f353b20c3fc7b7c
Flavio Ceolin <flavio.ceolin@profusion.mobi>
2012-05-05 10:27:48 -0300

    Fixing the exit script

--- scripts/wifi_exit.sh
+++ scripts/wifi_exit.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-killall udhcpd
+killall udhcpd 2>/dev/null
 
+PIDS=`ps | grep "udhcpc -i wlan0" | cut -d' ' -f2`
+echo "PIDS: $PIDS"
+
+for i in $PIDS; do
+    kill -9  $i 2>/dev/null
+done
